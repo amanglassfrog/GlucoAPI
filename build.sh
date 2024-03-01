e1ac57563aed876530d31a3a9ebc32e78f2961ee
amanglassfrog <135016564+amanglassfrog@users.noreply.github.com>
2024-03-01 15:40:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 --use-pep517 
 pip install -r requirements.txt
 
-pip install tensorrt
-#!pip install tensorflow-gpu==2.8.0
-pip install nvidia-pyindex
-pip install nvidia-tensorrt
+# pip install tensorrt
+# #!pip install tensorflow-gpu==2.8.0
+# pip install nvidia-pyindex
+# pip install nvidia-tensorrt
 
-cd ml_service
+# cd ml_service
 
 python3 manage.py migrate
 
